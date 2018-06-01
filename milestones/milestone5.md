402a66a0547df1873c320227826d8292e4a17c37
niralpatel <ndp002@ucsd.edu>
2018-06-01 01:12:44 -0700

    Installed and included plotly

---
+++ milestones/milestone5.md
@@ -0,0 +1,9 @@
+1. Screenshots of latest UI
+
+2. Improvements on UI from last time
+
+3. Screenshots of data visualization
+
+4. What's going on with the data visualization?
+
+5. demo
--- routes/courses.js
+++ routes/courses.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+
 /* GET course page */
 router.get('/:id', function (req, res) {
     switch (req.params.id) {
         case 'cogs121':
+            //res.send("the first");
             res.render('course', {name: 'COGS 121'});
             break;
         case 'cogs123':
+            //res.send("the second");
             res.render('course', {name: 'COGS 123'});
             break;
         case 'cogs102c':
+            //res.send("the third");
             res.render('course', {name: 'COGS 102C'});
             break;
     }
